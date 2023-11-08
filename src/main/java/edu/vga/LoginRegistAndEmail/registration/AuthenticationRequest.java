1d0bbcf50c22679607c7e58ef73aa355971781d9
Иван <ivanpaskhin@yandex.ru>
2023-11-08 18:18:41 +0300

    Register And login with Jwt.

---
+++ src/main/java/edu/vga/LoginRegistAndEmail/registration/AuthenticationRequest.java
@@ -0,0 +1,18 @@
+package edu.vga.LoginRegistAndEmail.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+
+
+}
---
+++ src/main/java/edu/vga/LoginRegistAndEmail/registration/RegisterRequest.java
@@ -0,0 +1,24 @@
+package edu.vga.LoginRegistAndEmail.registration;
+
+
+import edu.vga.LoginRegistAndEmail.appUser.AppUserRole;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+
+}
